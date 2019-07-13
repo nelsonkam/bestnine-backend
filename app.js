37139374b78ffc039574c10097a05b17e1982057
Nelson Kamga <kamganelson@gmail.com>
2019-07-13 01:34:13 +0100

    Fix cors issue

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require("cors")
 
 
 require('dotenv').config();
@@ -19,8 +19,9 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
 
-app.use("/", express.static(path.join(__dirname, 'public')));
+// app.use("/", express.static(path.join(__dirname, 'public')));
 
 app.use('/api', api);
 
@@ -37,7 +38,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500).json({
     message: err.message,
